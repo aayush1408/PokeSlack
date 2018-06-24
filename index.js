1121ad85b36ba6f1d5012e50de05bdd7e8585712
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-24 15:16:47 +0530

    major changes made

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
+const pokemon = require('pokemon');
 
 const bot = new SlackBot({
-    token:'xoxb-386225781716-386596628485-tRUKL7olfj3JqrCEzD8DLKhK',
-    name:'Pokebot'
+token:'xoxb-386225781716-386596628485-tRUKL7olfj3JqrCEzD8DLKhK',
+name:'Pokebot'
 });
 
 bot.on('start',()=>{
-    let params = {
-        icon_emoji:':smiley:'
-    };
-    bot.postMessageToChannel('general', 'Ask me all about your favourite pokemon!', params); 
+let params = {
+icon_emoji:':smiley:'
+};
+bot.postMessageToChannel('general', 'Ask me all about your favourite pokemon!', params); 
 });
 
 bot.on('error',(err)=>{
-    console.log(err);
+console.log(err);
 });
 
 //Message handler
 bot.on('message',data =>{
-    if (data.type !== "message") {
-        return;
-    }
-    console.log('I am the first event');
-    handleMessage(data.text);
+console.log(data);
+if (data.type !== "message" || data.subtype === 'bot_message' ){
+return;
+}
+else{
+handleMessage(data.text);
+console.log('Message');
+}
 });
 
 function handleMessage(message){
-    console.log('handle message');
-    if(message){
-        getPokemon(message);
-    }
+console.log('handle message');
+var msg = message.split(' '); 
+console.log(msg);
+msg.map((word)=>{
+console.log(word);
+let newWord = word[0].toUpperCase() + word.slice(1);
+console.log(newWord);
+let index = pokemon.all().indexOf(newWord);
+if( index !== -1 ){
+console.log(word,index);
+getPokemon(word,message);
+}
+else{
+console.log('What are you typing?');
+}
+});
 }
 
-async function getPokemon(pokemon){
-    console.log('I am astync func');
-    let params = {
-        icon_emoji:':sad:'
-    };
-    const res = await axios(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    secondQuestion(res.data);      
+async function getPokemon(pokemonName,message){
+console.log('I am async func');
+let params = {
+icon_emoji:':sad:'
+};
+const res = await axios(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+console.log(res.data);
+if(message.includes(' height')){
+pokemonHeight(res.data);
+}
 }
 
-function secondQuestion(pokemon){
-    console.log('Second ques');    
-    let params = {
-        icon_emoji:':smiley:'
-    };
-    bot.postMessageToChannel('general',`Okay, What you want to know about ${pokemon.name}?`,params);
-    bot.on('message',data =>{
-        if(data.type !== 'message'){
-            return;
-        }
-        console.log('Second event');    
-        if(data.text.includes('height')){
-            getDetails(data.text,pokemon)
-        }
-    });
-}
-
-function getDetails(character,pokemon){
-    console.log('Get details');
-    let params = {
-        icon_emoji:':smiley:'
-    };
-    let stats = pokemon[character];
-    bot.postMessageToChannel('general',`The ${character} of the ${pokemon.name} is ${stats}`,params);    
+function pokemonHeight(pokemonData){
+let params = {
+icon_emoji:':smiley:'
+};
+console.log(pokemonData.height); 
+bot.postMessageToChannel('general',`The height of the ${pokemonData.name} is ${pokemonData.height}`,params); 
 }
